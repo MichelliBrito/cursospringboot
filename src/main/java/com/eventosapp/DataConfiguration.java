a94b9c22f63c1fda12ee934ebf74fabc1ec653bb
michellibrito <michellidibrito@gmail.com>
2017-10-07 14:19:55 -0300

    adicionando profiles

--- src/main/java/com/eventosapp/DataConfiguration.java
+++ src/main/java/com/eventosapp/DataConfiguration.java
@@ -5,12 +5,14 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
+@Profile("dev")
 public class DataConfiguration {
 
 	@Bean
--- src/main/java/com/eventosapp/DataConfigurationPostgreSQL.java
+++ src/main/java/com/eventosapp/DataConfigurationPostgreSQL.java
@@ -6,9 +6,13 @@ import java.net.URISyntaxException;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+
 
 @Configuration
-public class DataConfigurationPostgreSQL {
+@Profile("prod")
+public class DataConfigurationPostgreSQL{
 
 	@Bean
     public BasicDataSource dataSource() throws URISyntaxException {

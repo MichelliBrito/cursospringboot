a6c208bf061cceeae79932f0fcc08f2e624ddafc
michellibrito <michellidibrito@gmail.com>
2017-11-10 15:56:58 -0200

    implementando autenticação em memória com Spring Security

---
+++ src/main/java/com/eventosapp/security/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.eventosapp.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable().authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin().permitAll()
+		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication()
+		.withUser("michelli").password("123").roles("ADMIN");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws java.lang.Exception{
+		web.ignoring().antMatchers("/materialize/**", "/style/**");
+	}
+}
